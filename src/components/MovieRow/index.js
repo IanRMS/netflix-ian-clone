c7fffbedd91e7628869bedfe90ccebef66ef96ac
IanCorebiz <ian.ribeiroms@gmail.com>
2020-12-29 10:25:09 -0300

    feat - header + footer + featured + row scroll

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import './style.css';
 
 export default ({title, items}) => {
+    const [scrollX, setScrollX] = useState(-400)
+
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2.5);
+        if ( x > 0) {
+            x = 0;
+        }
+        setScrollX(x);
+    }
+
+    const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2.5);
+        let listWidth = items.results.length * 150;
+        if ((window.innerWidth - listWidth) > x) {
+            x = (window.innerWidth - listWidth) - 60;
+        }
+        setScrollX(x);
+    }
+
     return (
-        <div>
+        <div className="movieRow">
             <h2>{title}</h2>
-            <div classname="movieRow--listarea">
-                {items.results.length && items.results.map((item, i) => (
-                    <img 
-                        src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} 
-                        alt={item.original_title} 
-                        key={i}
-                    />
-                ))}
+            <div className="movieRow--left" onClick={handleLeftArrow}>
+                <NavigateBeforeIcon style={{fontSize: 50}}/>
+            </div>
+            <div className="movieRow--right" onClick={handleRightArrow}>
+                <NavigateNextIcon style={{fontSize: 50}}/>
+            </div>
+            <div className="movieRow--listarea">
+                <div className="movieRow--list" 
+                    style={{
+                        marginLeft: scrollX, 
+                        width: items.results.length * 150
+                    }}
+                >
+                    {items.results.length && items.results.map((item, i) => (
+                        <div key={i} className="movieRow--item">
+                            <img 
+                                src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} 
+                                alt={item.original_title} 
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
